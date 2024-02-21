79cdf4bb767c5a4d53363b38eadce626f042df7f
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-02-21 12:22:14 -0300

    Update user_profile.css and profile.php, makes the profile picture round, Add a cover photo to the user profile.

--- simple/crowd-comments/site/php/profile.php
+++ simple/crowd-comments/site/php/profile.php
@@ -3,27 +3,43 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="../css/user_profile.css">
+        <?php include "section.php";?>
         <title>Profile</title>
 </head>
     <body>
 
-    <header></header>
+    <header>
+        <?php include "header.php";?>
+    </header>
     <main>
-        <h1><?php 
-        echo $_COOKIE['username']; 
-        ?></h1>
-        <header></header>
-        <main>
-
+        
+        <div class="user_profile_header">
+            <div class="pfp"><?php 
+                echo "<img class='usr_pfp' src='../users/" . $_COOKIE['username'] . "/pfp.png'>";?>
+            </div>
+            <div class="edit_pfp_div">
+            <!-- <p><?php echo $_COOKIE['username']?></p> -->
+            </div>
+            <div class="edit_picture_div">
+            <!--  -->
+            </div>
+            <div class="username">
+                <p><?php echo $_COOKIE['username']?></p>
+            </div>
+            
+        </div>
         <!-- TO DELETE
     THIS MUST BE ADDED TO THE EDTI PROFILE PAGE.-->
+    <!--
         <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
+             Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+           <input type="submit" value="Upload Image" name="submit">
 
-</form>
-        </main>
+        </form> 
+-->
     </main>
     <footer></footer>
     </body>
