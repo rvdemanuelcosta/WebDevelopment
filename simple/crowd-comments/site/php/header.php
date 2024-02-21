a974ff92adeb404b8128b228a13483ab82fd5d45
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-02-21 06:35:28 -0300

    Add a variable to store the server and path to the project root. Fix logo not displaying correctly. Fix user profile picture image not loading. Fix profile link not loading the correct address. Add the header.php to be used in other pages.

---
+++ simple/crowd-comments/site/php/header.php
@@ -0,0 +1,33 @@
+<?php
+include "section.php";
+$server_path = "http://" . $_SERVER['HTTP_HOST'] . "/WebDevelopment/simple/crowd-comments/site";
+// TO DELETE BELLOW
+/*
+if(substr(__DIR__, -3) === 'php'){
+    $project_root = dirname(__DIR__);
+}
+else if (substr(__DIR__, -4) === 'site'){
+    echo "root " . __DIR__;
+}
+*/
+// TO DELETE ABOVE
+echo "
+<a href='$server_path/index.php' id='logo'><img src='$server_path/images/logo.png' class='logo'></a>
+    <div>
+        <input type='text'> <input type='button' value='&#128270;'>
+        <link rel='stylesheet' href='../css/user_profile.css'>
+    </div> 
+        <div class='header-links'>";
+            if($user_cookie == null){
+                echo "<a href='login.html'>Login</a>
+                <a href='signup.html'>Sign up</a>";
+                }
+                else 
+                {
+                    
+                    echo "<img src='$user_pfp.png' id='user_pfp'><a href=" . $server_path . "/php/profile.php" . ">$user_cookie</a>
+                    <a href='" . $server_path . "/php/logout.php'>Log out</a>";
+                }
+                echo "<a href='#'>About us</a>
+    </div>";
+    ?>
