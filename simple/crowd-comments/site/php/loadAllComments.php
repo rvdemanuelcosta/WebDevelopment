693cc5dcd36be9d39f35020815b58fcba10e3005
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-02-27 22:25:58 -0300

    Add a div to better display the html code.

--- simple/crowd-comments/site/php/loadAllComments.php
+++ simple/crowd-comments/site/php/loadAllComments.php
@@ -7,11 +7,11 @@
     $conn = new mysqli($server, $user, $pass, $db);
     $result = $conn->query("select * from comments ORDER BY id DESC");
     while($row = $result->fetch_assoc()){
-        echo "
+        echo "<div class='comment-section fit-content-h'>
         <div class='comment'>
         <div class='pfp'><img src='users/$row[username]/pfp.png' class='user_pfp'></div><a href='profiles/" . $row["username"] . "'>"
             . $row['username'] . "</a><br>
-            <p>" . $row['comment'] . "</p></div>";
+            <p>" . $row['comment'] . "</p></div></div>";
     }
     $conn->close();
 ?>
