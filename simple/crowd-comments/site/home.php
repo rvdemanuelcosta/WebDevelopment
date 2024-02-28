1a827a871d86be8e7c9d7abd33d4c82781cae5d1
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-02-27 22:26:38 -0300

    Update home.php, add 3 butttons, 1 of the bottons show all comments if clicked.

--- simple/crowd-comments/site/home.php
+++ simple/crowd-comments/site/home.php
@@ -4,16 +4,27 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/buttons.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/loadAllComments.js"></script>
         <title>Home</title>
     </head>
     <body>
         <header><?php include "php/header.php";?>
         </header>
-        <main><?php 
-        if(file_exists("./index.php")){
-            echo substr($_SERVER["REQUEST_URI"], 43);
-        }
+        <main>
+            <div class="margin-h-auto fit-content-w">
+                <a class="link-bt">Following</a> 
+                <a class="link-bt">Tranding</a>
+                <a class="link-bt" onclick="loadComments()">All comments</a>
+            </div>
+            <div class="load-content vw-50 margin-h-auto">
+            <div class="load-content vw-50 margin-h-auto" id="commentSection">
+
+            </div>
+            <?php 
+        
          ?></main>
-        <footer></footer>
+        <footer><?php include "php/footer.php"; ?></footer>
     </body>
 </html>
