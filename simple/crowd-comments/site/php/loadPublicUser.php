1b70c27517b32c2067e8f2c94ec18a11092dcbf8
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-02-29 20:53:54 -0300

    Update loadPublicUser, add a div for user biography.

--- simple/crowd-comments/site/php/loadPublicUser.php
+++ simple/crowd-comments/site/php/loadPublicUser.php
@@ -1,6 +1,5 @@
 <?php 
 $username = $_GET['username'];
-    echo $username;
     echo "<div class='user_profile_header' style='background-image: url(../../users/$username/cover.png)';>
         <div class='pfp'>
             <img class='usr_pfp' src='../../users/$username/pfp.png'>
@@ -8,6 +7,22 @@ $username = $_GET['username'];
         <div>
             <p>$username</p>
         </div>
+    </div>
+    <div class='margin-h-auto vw-50' style='margin-top: 30px;'>
+        <a href='#' class='link-bt'>Follow</a>
+        <a href='#' class='link-bt'>Report</a>
+    </div>
+    <div class='bio-public vw-50 margin-h-auto'>
+        <h2>Biography</h2>"; 
+        /// check if user has or not a default bio and echo it in the <p> bellow.
+        if("a" == "a"){
+            echo "yes";
+        }
+        else {
+            echo "no";
+        }
+        echo "
+        <p>User did not wrote a biography.</p>
     </div>
     ";
 ?>
