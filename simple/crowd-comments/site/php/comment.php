ca74d2bfff7af96729f983ec465a1a4f42b03068
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-01 12:12:51 -0300

    Replace the  global for the  global variable, use a variable to store the comment.

--- simple/crowd-comments/site/php/comment.php
+++ simple/crowd-comments/site/php/comment.php
@@ -5,16 +5,21 @@ $password = "";
 $database = "crowd_comments";
 session_start();
 $conn = new mysqli($server, $username, $password, $database);
-$sql = "INSERT INTO comments (username, comment) VALUE ('$_SESSION[username]', '$_POST[comment_]');";
+$comment = $_GET['comment'];
+//echo $comment . "testteststestsdfg";
+
+$sql = "INSERT INTO comments (username, comment) VALUE ('$_SESSION[username]', '$comment');";
 if($conn->query($sql)){
     $log_file = fopen("../logs/log.txt", "a") or die("Unable to open log file.");
+    // get the user ip address
     if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != ''){
         $userIpAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
     } else { 
         $userIpAddress = $_SERVER['REMOTE_ADDR'];
     }
+    // write the log to file.
     $log = "Logged action: comment. \n" . "Username: " . $_SESSION['username'] . ", ID: " . $_SESSION['id'] . 
-    ", IP address: " . $userIpAddress . "\n comment: " . $_POST['comment_'] . "\n --------- \n";
+    ", IP address: " . $userIpAddress . "\n comment: " . $comment . "\n --------- \n";
     fwrite($log_file, $log);
     fclose($log_file);
     header("Location: ../index.php");
