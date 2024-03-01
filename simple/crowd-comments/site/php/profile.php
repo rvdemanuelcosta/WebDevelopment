bd5764548380e3d10467373b362583d557e14c36
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-01 12:10:06 -0300

    Delete old comments.

--- simple/crowd-comments/site/php/profile.php
+++ simple/crowd-comments/site/php/profile.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="../css/buttons.css">
         <link rel="stylesheet" href="../css/style.css">
         <script src="../js/loadUserComments.js"></script>
+        <script src="../js/postComment.js"></script>
         <title>Profile</title>
 </head>
     <body onload="loadComments()">
@@ -40,12 +41,12 @@
         </div>
         
         <div class="comment-area">
-        <form action="comment.php" method="post">
-            <p>Make a comment</p>
+        <form>
+            <p >Make a comment</p>
             <div class="comment-box">
-                <textarea name="comment_" maxlength="256"></textarea>
+                <textarea name="comment_" maxlength="256" id="commentToPost"></textarea>
             </div>
-            <input type="submit">
+            <a class="link-bt" style="margin: 10px;" onclick="postComment()">Comment</a>
         </form>
         </div>
         <div class="commentsToolBar vw-50 margin-h-auto">
@@ -53,25 +54,8 @@
             <input type="button" value="&#128260" onclick="loadComments()" id="reload-bt">
         </div>
         <div class="comment-section vw-50 margin-h-auto" id="commentSection">
-            <?php /*
-                $userComQuery = "SELECT * FROM comments WHERE username='$_SESSION[username]'";
-                $commResult = $conn->query($userComQuery);
-                while($row = $commResult->fetch_assoc()){
-                    echo "<div class='comment'><div class='pfp'><img src='../users/$_SESSION[username]/pfp.png' class='user_pfp'></div>
-                    <a href='../users/$_SESSION[username]'>$_SESSION[username]</a><br><p>$row[comment]</p></div>";
-                } */
-            ?>
+            
         </div>
-        <!-- TO DELETE
-    THIS MUST BE ADDED TO THE EDTI PROFILE PAGE.-->
-    <!--
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-             Select image to upload:
-            <input type="file" name="fileToUpload" id="fileToUpload">
-           <input type="submit" value="Upload Image" name="submit">
-
-        </form> 
--->
     </main>
     <footer><?php include "footer.php";?></footer>
     </body>
